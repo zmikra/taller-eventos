15109bc6c843519560ccc499ea9c234476580490
Micaela Rodríguez <mrodriguez111194@gmail.com>
2024-08-11 16:07:14 -0300

    events added

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let btn = document.getElementById('btn');
+    btn.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
